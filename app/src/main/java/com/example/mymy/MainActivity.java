bf24e881c83f9e8bca09a5c0684ecc9a3bfe48ab
cys42180 <cys42180@gmail.com>
2021-11-03 17:36:55 +0900

    First commit

---
+++ app/src/main/java/com/example/mymy/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.mymy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+
+
+//클라우드 서버로 만들었다
